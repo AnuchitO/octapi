3b3a00ecb06eb3db939db151428d44a9846d8120
AnuchitO <anuchit.prasertasng@gmail.com>
2020-11-07 13:46:57 +0700

    octapi

--- server.go
+++ server.go
@@ -9,10 +9,11 @@ func helloHandler(w http.ResponseWriter, req *http.Request) {
 	resp := []byte(`{"name": "anuchit"}`)
 	w.Write(resp)
 }
-
+// http://localhost:1234/
 func main() {
 	fmt.Println("start...")
 	http.HandleFunc("/", helloHandler)
 
+	http.ListenAndServe(":1234567890", nil) // ":1234567890"
 	fmt.Println("end....")
 }
