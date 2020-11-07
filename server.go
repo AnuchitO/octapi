03beee8bdba6f7b0c0ba347cf9ad61aaf09c894f
AnuchitO <anuchit.prasertasng@gmail.com>
2020-11-07 17:01:25 +0700

    octapi

--- server.go
+++ server.go
@@ -6,15 +6,43 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func helloHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "hello",
-	})
+type Todo struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Status string `json:"status"`
+}
+
+var todos = map[int]*Todo{
+	1: &Todo{ID: 1, Title: "pay phone bills", Status: "active"},
+}
+
+func getTodosHandler(c *gin.Context) {
+	items := []*Todo{}
+	for _, item := range todos {
+		items = append(items, item)
+	}
+	c.JSON(http.StatusOK, items)
+}
+
+func createTodosHandler(c *gin.Context) {
+	t := Todo{}
+	if err := c.ShouldBindJSON(&t); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	id := len(todos)
+	id++
+	t.ID = id
+	todos[t.ID] = &t
+
+	c.JSON(http.StatusCreated, "created todo.")
 }
 
 func main() {
 	r := gin.Default()
-	r.GET("/hello", helloHandler)
+	r.GET("/todos", getTodosHandler)
+	r.POST("/todos", createTodosHandler)
 
-	r.Run(":1234")                       // listen and serve on 127.0.0.0:8080
+	r.Run(":1234")
 }
